2ee18d5e48a6e0cd3f1ef1ea9aff2eeb6a861a5a
Son Bui <ngocson.bui0312@gmail.com>
2025-04-12 17:58:04 +0300

    finalize steps app

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
+const Steps = (props) => {
+  const { steps, active } = props;
+  return [...Array(steps).keys()].map((i) => (
+    <div className={i <= active && 'active'} key={'steps' + i}>
+      {i + 1}
+    </div>
+  ));
+};
+
+const App = () => {
+  const [step, setStep] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        <Steps steps={3} active={step} />
+      </div>
+      <p className="message">{messages[step]}</p>
+      <div className="buttons">
+        <button
+          style={{ background: '#7950f2', color: '#fff' }}
+          onClick={() => {
+            setStep((i) => (i === 0 ? 0 : i - 1));
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button
+          style={{ background: '#7950f2', color: '#fff' }}
+          onClick={() => {
+            setStep((i) => (i === 2 ? 2 : i + 1));
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+/*
+1. state -> data that will change
+2. component -> reflection of state
+3. dont use state for things that do not trigger rerender
+*/
 
 export default App;
